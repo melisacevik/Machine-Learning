c2b3daf9b437c3c6bb7d16588af5223f555ced00
Melisa Çevik <celissamevik@gmail.com>
2024-03-15 16:48:00 +0300

    add CART

---
+++ cart.py
@@ -0,0 +1,64 @@
+################################################
+# Decision Tree Classification: CART
+################################################
+
+# 1. Exploratory Data Analysis
+# 2. Data Preprocessing & Feature Engineering
+# 3. Modeling using CART
+# 4. Hyperparameter Optimization with GridSearchCV
+# 5. Final Model
+# 6. Feature Importance
+# 7. Analyzing Model Complexity with Learning Curves (BONUS)
+# 8. Visualizing the Decision Tree
+# 9. Extracting Decision Rules
+# 10. Extracting Python/SQL/Excel Codes of Decision Rules
+# 11. Prediction using Python Codes
+# 12. Saving and Loading Model
+
+
+# pip install pydotplus
+# pip install skompiler
+# pip install astor
+# pip install joblib
+
+import warnings
+import joblib
+import pydotplus
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, export_text
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, validation_curve
+from skompiler import skompile
+import graphviz
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+warnings.simplefilter(action='ignore', category=Warning)
+
+################################################
+# 1. Exploratory Data Analysis
+################################################
+
+################################################
+# 2. Data Preprocessing & Feature Engineering
+################################################
+
+################################################
+# 3. Modeling using CART
+################################################
+
+df = pd.read_csv("datasets/diabetes.csv")
+
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+cart_model = DecisionTreeClassifier(random_state=1).fit(X, y)
+
+# Confusion matrix için y_pred:
+cart_model = cart_model.predict(X)
+
+#roc eğrisi için 1.sınıfa ait olma olasılıkları gerekiyor.
